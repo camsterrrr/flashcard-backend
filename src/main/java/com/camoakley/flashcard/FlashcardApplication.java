95a2a5fa5b82dca9d415c98510a2d4ec514eaba9
camoakley <oakley.cameronj@gmail.com>
2023-08-08 20:58:20 -0700

    first commit

---
+++ src/main/java/com/camoakley/flashcard/FlashcardApplication.java
@@ -0,0 +1,10 @@
+package com.camoakley.flashcard;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashcardApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(FlashcardApplication.class, args);
+	}
+}
